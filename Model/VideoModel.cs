7b32e492430d57b8993dbc840e7d62e18baaf637
Bálint Áts <atsbalint@gmail.com>
2020-09-24 18:57:51 +0200

    Create video model

---
+++ Model/VideoModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoLister.Model
+{
+    class VideoModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public string Tags { get; set; }
+        public string ProfileImage { get; set; }
+        public string PreviewImages { get; set; }
+        public string TargetUrl { get; set; }
+        public string DetailsUrl { get; set; }
+        public string Quality { get; set; }
+        public string IsHd{ get; set; }
+        public string Uploader { get; set; }
+        public string UploaderLink { get; set; }
+    }
+}
