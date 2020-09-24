28fc0b20f2b53d4f47173716850df0ccf2e52ace
Bálint Áts <atsbalint@gmail.com>
2020-09-24 16:12:19 +0200

    Create Api.cs and GET method

---
+++ API/api.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoLister.API
+{
+    class Api
+    {
+
+        protected HttpResponseMessage GET(string url)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                
+                var response = client.GetAsync(url);
+                response.Wait();
+
+                return response.Result;
+            }
+
+            return null;
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+
 
 namespace VideoLister
 {
@@ -23,6 +25,7 @@ namespace VideoLister
         public MainWindow()
         {
             InitializeComponent();
+            Trace.WriteLine("main window initialized");
         }
     }
 }
